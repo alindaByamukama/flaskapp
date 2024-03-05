fa2d185786af4edb9ec3905f9aa4172b0d66d060
Alinda Byamukama <alindabyamukama@gmail.com>
2024-03-05 13:12:44 +0300

    add url building: display user profile endpoint w username

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import url_for
 app = Flask(__name__)
 
 # route for home "/"
@@ -21,6 +22,16 @@ def posts():
 def post_id():
     # logic to recieve and display blog post with given post ID
     return f'Blog Post #{post_id}'
+
+#  a user profile that accepts a variable username
+@app.route("/user/<username>")
+def user_profile(username):
+    # display user profile based on username
+    return f'{username}\'s profile'
+
+# generate a url for user_profile endpoint w username alice
+with app.test_request_context():
+    print(url_for('user_profile', username='alice'))
 
 if __name__ == '__main__':
     app.run()
