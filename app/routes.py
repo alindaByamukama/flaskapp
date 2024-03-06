57234e01031aac09146412f1c633f21b17f7040f
Alinda Byamukama <alindabyamukama@gmail.com>
2024-03-06 12:13:38 +0300

    add render template to simply render the homepage template

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Susan'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Flaskblog</title>
-    </head>
-    <body>
-        <h1>Welcome, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title='Home', user=user)
