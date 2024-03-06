36d3fbda10e84f0d7c8c976ec0074002c6d82939
Alinda Byamukama <alindabyamukama@gmail.com>
2024-03-06 12:05:56 +0300

    add a home page view

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    user = {'username': 'Susan'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Flaskblog</title>
+    </head>
+    <body>
+        <h1>Welcome, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
