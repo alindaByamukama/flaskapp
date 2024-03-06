cc04189cf96bed33eebc97b8900fde57fb5bb9cc
Alinda Byamukama <alindabyamukama@gmail.com>
2024-03-06 12:27:26 +0300

    add mock posts

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Susan'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beatiful day in Jinja!'
+        },
+        {
+            'author': {'username': 'Martha'},
+            'body': 'Just watched Dune at Cineplex, so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
