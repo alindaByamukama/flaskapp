7078ff562009da35e6ca29107362e8aa4a1e5e98
Alinda Byamukama <alindabyamukama@gmail.com>
2024-03-13 15:46:56 +0300

    add shell context configuration that adds db instance and models to shell session

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-from app import app
+# a python script at the top level that defines the flask application instance
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Post
+
+# creates a shell context 
+# adds the database instance and models to the shell session
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
