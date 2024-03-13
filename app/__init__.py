3084861e561f9e378d3e7a2e03c27ca7f5689033
Alinda Byamukama <alindabyamukama@gmail.com>
2024-03-13 12:49:38 +0300

    edit typo in sqlalchemy import

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
