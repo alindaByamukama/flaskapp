1ba51d698215a7e95c218536f0891fffc838eb7c
Alinda Byamukama <alindabyamukama@gmail.com>
2024-03-13 17:31:37 +0300

    add flask login mixin class - usermixin

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from typing import Optional
@@ -5,7 +6,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
