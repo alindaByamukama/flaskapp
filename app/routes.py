b6d614555bf7bbd72f39bc094de3cf8b83a615a7
Alinda Byamukama <alindabyamukama@gmail.com>
2024-03-13 18:08:30 +0300

    add complete login logic

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -20,9 +24,21 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # in case the user is already logged in they are redirected to index
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        # load user from db
+        user = db.session.scalar(
+            # query the db w form submission info to find user
+            sa.select(User).where(User.username == form.username.data))
+        # check the password provided by the form
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        # register user as logged in 
+        # current user var is set to logged in user
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
