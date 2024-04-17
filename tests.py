0d546f4a8c828e507ebcfb4d5b9b1094cc083fdf
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-17 15:50:43 +0300

    add user model test case, add setup method, add info comments

--- tests.py
+++ tests.py
@@ -1,7 +1,15 @@
 import os
+# uses an in memory sqlite db during tests
 os.environ['DATABASE_URL'] = 'sqlite://'
 
 from datetime import datetime, timezone, timedelta
 import unittest
 from app import app, db
 from app.models import User, Post
+
+class UserModelCase(unittest.TestCase):
+    # setup method creates an application context and pushes it
+    def setUp(self) -> None:
+        self.app_context = app.app_context()
+        self.app_context.push()
+        db.create_all()
