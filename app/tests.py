8a8d44c98c863d6c482bfcdcda4f51d4902bfc19
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-17 15:46:46 +0300

    add imports

--- app/tests.py
+++ app/tests.py
@@ -1,2 +1,7 @@
 import os
 os.environ['DATABASE_URL'] = 'sqlite://'
+
+from datetime import datetime, timezone, timedelta
+import unittest
+from app import app, db
+from app.models import User, Post
