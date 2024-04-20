581daa81430718a9cd7f38577a2cd25d916c5705
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-20 16:03:00 +0300

    add items attribute to posts obj line 101

--- app/routes.py
+++ app/routes.py
@@ -98,7 +98,7 @@ def user(username):
     prev_url = url_for('user', page=posts.prev_num) \
         if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form, posts=posts, next_url=next_url, prev_url=prev_url)
+    return render_template('user.html', user=user, posts=posts.items, form=form, next_url=next_url, prev_url=prev_url)
 
 @app.before_request
 def before_request():
