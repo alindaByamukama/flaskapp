f6ae0a1cc931f1872de7b41650eebea0a6b030f0
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-20 15:37:51 +0300

    add next and previous links to index page pagination

--- app/routes.py
+++ app/routes.py
@@ -22,7 +22,11 @@ def index():
         return redirect(url_for('index'))
     page = request.args.get('page', 1, type=int)
     posts = db.paginate(current_user.following_posts(), page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    return render_template('index.html', title='Home', form=form, posts=posts.items)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @app.route('/explore')
 @login_required
