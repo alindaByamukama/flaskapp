32aa707e5ab25b13a5719938b9bc11d880cfc428
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-20 13:00:30 +0300

    add posts var to display real post in home page

--- app/routes.py
+++ app/routes.py
@@ -20,7 +20,7 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-
+    posts = db.session.scalars(current_user.following_posts()).all()
     return render_template('index.html', title='Home', form=form, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
