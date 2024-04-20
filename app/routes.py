43eacea7e803e6202ae243c90dd82a327fcd469b
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-20 15:39:17 +0300

    add next and previous links to explore page pagination

--- app/routes.py
+++ app/routes.py
@@ -34,8 +34,12 @@ def explore():
     page = request.args.get('page', 1, type=int)
     query = sa.select(Post).order_by(Post.timestamp.desc())
     posts = db.paginate(query, page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
     # reused the index template, did not include the form arg
-    return render_template('index.html', title='Explore', posts=posts)
+    return render_template('index.html', title='Explore', posts=posts, next_url=next_url, prev_url=prev_url)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
